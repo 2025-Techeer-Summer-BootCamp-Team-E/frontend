9d137a6acdc8ea8af324dc8937de586ec5effc3a
seunggonhyeon <hyunsg5638@gmail.com>
2025-07-11 21:27:48 +0900

    design:공통 버튼 컴포넌트 퍼블리싱

--- src/pages/MyLibraryPage.tsx
+++ src/pages/MyLibraryPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TempComponent from "../components/TempComponent";
+import CommonButton from "../components/CommonButton";
 
 const MyLibrary: React.FC = () => {
   return (
     <div>
-      MyLibrary
+      MyLibraryㅇㅇ
       <TempComponent />
+      <CommonButton />
     </div>
   );
 };
