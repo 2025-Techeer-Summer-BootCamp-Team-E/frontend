cdc6af9db1ac72b2e5e22d28098bee3a8510ffa8
siillvergun <duejql03@naver.com>
2025-07-15 23:47:22 +0900

    위치, 크기 수정

--- src/components/BackCharacterCard.tsx
+++ src/components/BackCharacterCard.tsx
@@ -8,11 +8,11 @@ interface BackProps {
 const BackCharacterCard: React.FC<BackProps> = ({name, description}) => {
 
   return (
-    <div className="w-[264px] h-[300px] bg-[#9B8B7A] rounded-md shadow-[10px_10px_32px_0_rgba(0,0,0,0.16)] flex flex-col items-center relative ">
-      <div className="absolute top-2 font-bold text-[32px]">{name}</div>
-      <div className= "absolute top-10 w-[235px] h-[7px] bg-black flex items-center justify-center"></div>
+    <div className="w-[264px] h-[300px] bg-[#9B8B7A] rounded-md shadow-lg flex flex-col items-center">
+      <div className="font-bold text-[32px]">{name}</div>
+      <div className="w-[235px] h-[7px] bg-black flex items-center justify-center"></div>
       <div className="flex-1 flex items-center justify-center text-center px-2">
-        <span className="absolute top-14 w-[207px] h-[173px] text-[16px] text-white text-bold">{description}</span>
+        <span className="w-[207px] h-[173px] text-[16px] text-white text-bold">{description}</span>
       </div>
     </div>
   );
--- src/components/VideoInfo.tsx
+++ src/components/VideoInfo.tsx
@@ -10,7 +10,7 @@ export interface VideoInfoProps {
 
 const VideoInfo: React.FC<VideoInfoProps> = ({imageUrl, title, from, character, description}) => {
     return (
-        <div className="w-[1206px] [h-256px] flex rounded-[16px] border border-gray-300 bg-white shadow-md overflow-hidden max-w-3xl">
+        <div className="w-[1206px] [h-256px] flex rounded-[16px] border border-gray-300 bg-white shadow-md overflow-hidden">
             {/* 왼쪽: 이미지 */}
             <div className="flex-shrink-0 p-3">
              {imageUrl ? ( <img src={imageUrl} alt="Video thumbnail" className="w-[398px] h-[224px] border-2 rounded-[12px] object-cover" style={{ borderColor: "#AEAEAE" }}/>
@@ -19,9 +19,9 @@ const VideoInfo: React.FC<VideoInfoProps> = ({imageUrl, title, from, character, 
             
             {/* 오른쪽: 텍스트 */}
             <div className="flex flex-col justify-center flex-1 gap-2">
-                <div className="text-[20px] font-bold">{title}</div>
+                <div className="text-[24px] font-bold">{title}</div>
                 {from && <p className="text-[18px] text-gray-500 mb-4 font-semibold">from: {from}</p>}
-                <div className="w-[700px] h-[86px] rounded-[12px] bg-gray-100 px-2">
+                <div className="w-[700px] h-[95px] rounded-[12px] bg-gray-100 px-2">
                     <span className="font-bold text-[16px]">Character:</span>
                     <span className="font-bold text-[16px]">{character}</span>
                     <div className="text-gray-700 text-[16px]">{description}</div>
