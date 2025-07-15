1c0b396782a77ddccd51df0084935db766765a54
KimYeJin1004 <kimyeajin@inu.ac.kr>
2025-07-15 23:22:04 +0900

    design: Book 퍼블리싱

--- src/pages/MyLibraryPage.tsx
+++ src/pages/MyLibraryPage.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import TempComponent from "../components/TempComponent";
 import CommonButton from "../components/CommonButton";
 import Toggle from "../components/Toggle";
+import BooksSection from "../components/BooksSection";
 
 const MyLibrary: React.FC = () => {
   return (
     <div>
+      <BooksSection />
       <Toggle />
       <TempComponent />
       <CommonButton />
