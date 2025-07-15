61ba9780661ba332edea539311c274737b5a08f3
seunggonhyeon <hyunsg5638@gmail.com>
2025-07-15 17:10:00 +0900

    refactor:공통 버튼 재사용성 및 확장성 개선

--- src/pages/MyLibraryPage.tsx
+++ src/pages/MyLibraryPage.tsx
@@ -3,12 +3,15 @@ import CommonButton from "../components/CommonButton";
 import ThumbnailFetch from "../components/ThumbnailFetch";
 import Toggle from "../components/Toggle";
 import VideoInfoFetch from "../components/VideoInfoFetch";
+import BackIcon from "../assets/Icons/BackIcon.svg";
 
 const MyLibrary: React.FC = () => {
   return (
     <div>
       <Toggle />
-      <CommonButton />
+      <CommonButton icon={<img src={BackIcon} />}>
+        대본으로 돌아가기
+      </CommonButton>
       <ThumbnailFetch />
       <VideoInfoFetch />
     </div>
